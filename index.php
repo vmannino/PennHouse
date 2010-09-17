9f9de7e8e5d519ed8ccbe6714ba2c6871881b4df
unknown <Justin@.(none)>
2010-09-17 19:17:07 -0400

    Hello!

--- index.php
+++ index.php
@@ -1,6 +1,95 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-<title>Your Page Title</title>
-</head>
-<body>
-</body>
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<title>opennhouse | <?php //echo $page; ?></title>
+		
+		
+		<!-- Begin Stylesheets -->
+			<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<!-- End Stylesheets -->
+		
+		<!-- Begin JavaScript -->
+		<!-- End JavaScript -->
+			 
+	</head>
+	<body>
+	
+	
+		<div id="header"> 
+		
+			<div id="nav-bar"> 
+		
+				<div id="nav-left"> 
+		
+					<ul>
+						<li><a href="#">Welcome Justin!</a></li>
+						<li><a href="#">Dashboard</a></li>
+						<li><a href="#">Logout</a></li>
+					</ul>
+				</div>
+				
+				<div id="logo"> 
+		
+				</div>
+				
+				<div id="nav-right"> 
+					<ul>
+						
+						<li><a href="#">Find Houses</a></li>
+						<li><a href="#">Browse</a></li>
+						<li><a href="#">Find Landlords</a></li>
+					</ul>
+				</div>
+		
+		
+		
+			</div>
+		
+		
+		
+		</div>
+		
+		<div id="content"> 
+
+			
+			
+			<div id="map-canvas"> 
+		
+
+		
+				<div id="map-overlay"> 
+					
+
+				</div>
+			</div>
+		
+			<div id="search-results"> 
+		
+
+		
+				
+			</div>
+		
+			<div id="favorites"> 
+		
+
+		
+				
+			</div>
+			<div class="clear"></div>
+		
+		
+		</div>
+		
+		
+		<div id="footer"> </div>
+		
+		
+	</body>
+	
+	
+	
+	
+	
+</html>
