c35c9e145241cb1b1aa907f6d15d4830da1cfed6
unknown <vincemannino@gmail.com>
2010-09-18 02:13:07 -0400

    justin sucks

--- functions.php
+++ functions.php
@@ -8,7 +8,21 @@ function sanitize($string){
 	return mysql_real_escape_string($string);
 }
 
-
+function get_facebook_cookie($app_id, $application_secret) {
+  $args = array();
+  parse_str(trim($_COOKIE['fbs_' . $app_id], '\\"'), $args);
+  ksort($args);
+  $payload = '';
+  foreach ($args as $key => $value) {
+    if ($key != 'sig') {
+      $payload .= $key . '=' . $value;
+    }
+  }
+  if (md5($payload . $application_secret) != $args['sig']) {
+    return null;
+  }
+  return $args;
+}
 
 function getHeader($page, $title) {
 
--- index.php
+++ index.php
@@ -1,82 +1,27 @@
 <?php 
+	include_once("config.php");
 	include_once("functions.php");
 	getHeader("HOME","Home");
+	
+	$cookie = get_facebook_cookie(FACEBOOK_APP_ID, FACEBOOK_SECRET);
+
 ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+<body>
+    <?php if ($cookie) { ?>
+      Your user ID is <?= $cookie['uid'] ?>
+    <?php } else { ?>
+      <fb:login-button></fb:login-button>
+    <?php } ?>
+
+    <div id="fb-root"></div>
+    <script src="http://connect.facebook.net/en_US/all.js"></script>
 
 		
 		<div id="content"> 
 
 			
-			<div id="map-wrapper">
-				<div id="map-overlay"> 
-					<ul>
-						<li><a href="#filters">Filters</a></li>
-						<li><a href="#saved-searches">Saved Searches</a></li>
-					</ul>
-					
-					<div id="filters">
-						<h1>This is where Filters would go.</h1>
-
-						<form>
-						<input type="text" class="numInput" name="avgRentMin">
-						<input type="text" name="avgRentMax">
-						<input type="text" name="ttlRentMin">
-						<input type="text" name="ttlRentMax">
-						<input type="text" name="llRtngMin">
-						<input type="text" name="llRtngMax">
-						<input type="text" name="hsRtngMin">
-						<input type="text" name="hsRtngMax">
-						<input type="text" name="bdrmMin">
-						<input type="text" name="bdrmMax">
-						</form>
-						
-						<a id="filter-button" class="button bluebutton medium">Filter Results</a>
-					</div>
-					
-					<div id="saved-searches">
-						<h1>This is where saved searches would go.</h1>
-						
-					</div>
-
-				</div>
-				<div id="map-canvas">
-			
-					
-				</div>
-			</div>
-			
-		
-			<div id="search-results"> 
-			
-					<ul>
-						<li><a href="#results">Search Results</a></li>
-						<li><a href="#compare">Compare Houses</a></li>
-					</ul>
-					
-					<div id="results">
-						<h1>This is where detailed results would go.</h1>
-						
-					</div>
-					
-					<div id="compare">
-						<h1>This is where comparisons would go.</h1>
-						
-					</div>
-
-		
-				
-			</div>
-		
-			<div id="favorites" class="tabs"> 
-				<ul>
-					<li><a href="#tabs-1">Favorites</a></li>
-				</ul>
-				
-		
-			</div>
-			<div class="clear"></div>
-		
-		
 		</div>
 		
 		
