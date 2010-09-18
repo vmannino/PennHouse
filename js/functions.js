e412a56d2e2b6631055adfb2c8a3385ca3c9544d
unknown <Alex@.(none)>
2010-09-17 21:50:48 -0400

    no changes... again

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,22 @@
 functions.js
 
 function initialize() {
-			var latlng = new google.maps.LatLng(-34.397, 150.644);
-			var myOptions = {
-				zoom: 8,
-				center: latlng,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-			var map = new google.maps.Map(document.getElementById("map-canvas"),
-				myOptions);
-				
-			<!-- Needed later for translating adresses to LatLngs -->
-			var geocoder = new google.maps.Geocoder();
-		}
+	var latlng = new google.maps.LatLng(-34.397, 150.644);
+	var myOptions = {
+		zoom: 8,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"),
+		myOptions);
 		
-		$(document).ready(function(){
-			// Tabs
-			$('#search-results').tabs();
-			$('#map-overlay').tabs();
-		});	
+	<!-- Needed later for translating adresses to LatLngs -->
+	var geocoder = new google.maps.Geocoder();
+}
+		
+$(document).ready(function(){
+	// Tabs
+	$('#search-results').tabs();
+	$('#map-overlay').tabs();
+});
+
